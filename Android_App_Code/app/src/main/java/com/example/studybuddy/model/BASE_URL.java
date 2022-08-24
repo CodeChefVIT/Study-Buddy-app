2459aee96d3b0bf0739c6fa6c0e0946a3fd7dffa
Aryaman Awasthi <aryaman.awasthi79@gmail.com>
2022-08-25 00:04:25 +0530

    [FIX] Updated BASEURL

--- Android_App_Code/app/src/main/java/com/example/studybuddy/model/BASE_URL.java
+++ Android_App_Code/app/src/main/java/com/example/studybuddy/model/BASE_URL.java
@@ -1,7 +1,7 @@
 package com.example.studybuddy.model;
 
 public class BASE_URL {
-    private static final String BASE_URL = "http://152.67.22.250:6530";
+    private static final String BASE_URL = "https://codechef-study-buddy.fly.dev";
 
     public static String getBaseUrl() {
         return BASE_URL;
